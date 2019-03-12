926173eae20dfa8cbbe17ad2ee8d9cf2941d5e33
HUMANIAM <eng.i.saad94@gmail.com>
2019-03-12 14:48:29 +0200

    escape chars

---
+++ OS/shell_script/escapechars.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#escape chars using \ to mark these character to not be interpreted by the shell
+cat << EOF
+He said to me in one of my dreams 
+"The main key to success and happiness is never give up even you will die"
+EOF
+
+echo "escape characters are \$, \\, \" and there are many"
